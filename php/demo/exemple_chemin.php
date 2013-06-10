3ee758f52dbcdd526f543634101a243031ff35cb
mjcdamville <m.j.c.damville@orange.fr>
2013-06-10 17:28:27 +0300

    Create exemple_chemin.php

---
+++ php/demo/exemple_chemin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exemple requête GET</title>
+        <link rel="stylesheet" href="../../css/style.css"/>        
+    </head>
+
+    <body>
+      <h1>Une autre page</h1>
+      <p>Uniquement pour montrer l'usage des .. dans le chemin de la feuille de syle</p>
+    </body>
+</html>
